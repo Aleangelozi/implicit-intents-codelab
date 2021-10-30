985808c264331bdb2de365c31e71cafdd7ca757a
aleangelozi <aleangelozi@gmail.com>
2021-10-30 18:07:26 -0300

    Initial commit

---
+++ app/src/main/java/com/aleangelozi/implicitintents/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.aleangelozi.implicitintents
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var photo :ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        photo = findViewById(R.id.imageView)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it, 0)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == 0) {
+            val uri = data?.data
+            photo.setImageURI(uri)
+        }
+    }
+}
